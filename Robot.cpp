cfc58044b86c86034566bdbfb01e0256391c0b37
Michał Korybalski <korybalski97@gmail.com>
2021-06-11 23:53:45 +0200

    robot moves as second order system in angle space

--- Robot.cpp
+++ Robot.cpp
@@ -1,26 +1,31 @@
 #include "Robot.h"
 
 Robot::Robot(REAL arm1_length_, REAL arm2_length_, REAL angle1_deg_, REAL angle2_deg_) :
-	arm1_length(arm1_length_), arm2_length(arm2_length_), angle1_deg(angle1_deg_), angle2_deg(angle2_deg_),
+	arm1_length(arm1_length_), arm2_length(arm2_length_), angle1(angle1_deg_), angle2(angle2_deg_),
 	pen1(Color(100, 100, 100), arm_width), pen2(Color(70, 70, 70), arm_width),
 	base_pos(30, 30)
 {
 	pen1.SetLineCap(LineCapRound, LineCapRound, DashCapRound);
 	pen2.SetLineCap(LineCapRound, LineCapRoundAnchor, DashCapRound);
 
-	set_arms(angle1_deg, angle2_deg);
+	set_arms(angle1, angle2);
+	angle1 = angle1;
+	angle2 = angle2;
+	set_tAngles(angle1, angle2);
 
+	stopped = false;
+	tpos_reached = false;
 }
 
 void Robot::draw(Graphics* graphics)
 {
 	graphics->DrawLine(&pen1, base_pos, base_pos + arm1);
+	graphics->DrawLine(&pen1, base_pos + PointF(0, 20), base_pos + arm1 + arm2*0.05);
 	graphics->DrawLine(&pen2, base_pos + arm1, base_pos + arm1 + arm2);
 }
 
 void Robot::catch_triangle(Triangle* tri)
 {
-
 	if (catched_triangle == nullptr)
 	{
 		if (Triangle::is_point_int_triangle(tri, base_pos + arm1 + arm2))
@@ -37,23 +42,47 @@ void Robot::catch_triangle(Triangle* tri)
 }
 
 void Robot::update(REAL dt)
-{
+{	
+	ang_vel1 += (tAngle1 - 2 * dzeta1 * omega1 * ang_vel1 - angle1) * dt;
+	angle1 += omega1 * omega1 * ang_vel1 * dt;
+	ang_vel2 += (tAngle2 - 2 * dzeta2 * omega2 * ang_vel2 - angle2) * dt;
+	angle2 += omega2 * omega2 * ang_vel2 * dt;
+	
+	stopped = false;
+	tpos_reached = false;
+
+	float dang1 = abs(angle1 - tAngle1), dang2 = abs(angle2 - tAngle2);
+	if ( dang1 * dang1 + abs(ang_vel1) * abs(ang_vel1) < 25 &&
+		 dang2 * dang2 + abs(ang_vel2) * abs(ang_vel2) < 25)
+	{
+		this->stopped = true;
+	}
+
+	if (abs(angle1 - tAngle1) < 5 && abs(angle2 - tAngle2) < 5)
+	{
+		this->tpos_reached = true;
+	}
+
+
+	set_arms(angle1, angle2);
+	
+	if (this->following_trajectory)
+	{
+		this->follow_trajectory();
+	}
+
+
 	if (catched_triangle != nullptr)
 	{
 		catched_triangle->vel = (base_pos + arm1 + arm2 - catched_triangle->pos) * 10e-3;
-
-		//catched_triangle_r = base_pos + arm1 + arm2 - catched_triangle->pos;
-		//catched_triangle->collision_with_static_figure(catched_triangle->vel, catched_triangle->omega, catched_triangle_r);
 	}
 }
 
 void Robot::set_arms(REAL angle1, REAL angle2)
 {
-	angle1_deg = angle1;
-	angle2_deg = angle2;
-	Matrix m1, m2;
-	m1.Rotate(angle1_deg);
-	m2.Rotate(angle2_deg);
+	Gdiplus::Matrix m1, m2;
+	m1.Rotate(angle1);
+	m2.Rotate(angle2);
 
 	PointF vec1(arm1_length, 0), vec2(arm2_length, 0);
 	arm1 = { arm1_length, 0 };
@@ -69,14 +98,83 @@ void Robot::set_postion(PointF p)
 	REAL a1 = std::asin(p.Y / abs(p));
 	REAL a2 = std::acos((-std::pow(arm1_length, 2) + std::pow(arm2_length, 2) + std::pow(abs(p), 2)) / (2 * abs(p) * arm2_length));
 	REAL a3 = std::acos((std::pow(arm1_length, 2) - std::pow(arm2_length, 2) + std::pow(abs(p), 2)) / (2 * abs(p) * arm1_length));
-	angle1_deg = (a1 - a3) * 360 / 6.28;
-	angle2_deg = (a1 + a2) * 360 / 6.28;
-	angle2_deg -= angle1_deg;
-	set_arms(angle1_deg, angle2_deg);
+	angle1 = (a1 - a3) * 360 / 6.28;
+	angle2 = (a1 + a2) * 360 / 6.28;
+	angle2 -= angle1;
 
+}
+
+Vector2f Robot::calculateAngles(PointF p)
+{
+	float alpha1, alpha2;
+	p = p - base_pos;
+	REAL a1 = std::asin(p.Y / abs(p));
+	REAL a2 = std::acos((-std::pow(arm1_length, 2) + std::pow(arm2_length, 2) + std::pow(abs(p), 2)) / (2 * abs(p) * arm2_length));
+	REAL a3 = std::acos((std::pow(arm1_length, 2) - std::pow(arm2_length, 2) + std::pow(abs(p), 2)) / (2 * abs(p) * arm1_length));
+	alpha1 = (a1 - a3) * 360 / 6.28;
+	alpha2 = (a1 + a2) * 360 / 6.28;
+	alpha2 -= alpha1;
+	return Vector2f(alpha1, alpha2);
 }
 
 PointF Robot::get_positon()
 {
 	return base_pos + arm1 + arm2;
 }
+
+void Robot::set_tAngles(float a1, float a2)
+{
+	tAngle1 = a1;
+	tAngle2 = a2;
+}
+
+void Robot::set_tPosition(PointF p)
+{
+	Vector2f v1 = this->calculateAngles(p);
+	this->set_tAngles(v1[0], v1[1]);
+}
+
+void Robot::follow_trajectory()
+{
+	if (curr_tpos->robotCommand == rc_do_nothing)
+	{
+		if (this->tpos_reached)
+		{
+			curr_tpos++;
+		}
+	}
+	else if (curr_tpos->robotCommand == rc_stop)
+	{
+		if (this->stopped)
+		{
+			curr_tpos++;
+		}
+	}
+	else if (curr_tpos->robotCommand == rc_catch)
+	{
+		if (this->stopped)
+		{
+			curr_tpos++;
+		}
+	}
+
+	if (curr_tpos != this->trajectory.cend())
+	{
+		this->set_tPosition(PointF(curr_tpos->pos.X, curr_tpos->pos.Y));
+	}
+	else
+	{
+		this->following_trajectory = false;
+	}
+}
+
+void Robot::enter_trajectory(const std::vector<RobotPosition>& t)
+{
+	this->trajectory = t;
+	this->curr_tpos = this->trajectory.cbegin();
+	if (this->trajectory.size() != 0)
+		this->set_tPosition(PointF(curr_tpos->pos.X, curr_tpos->pos.Y));
+	this->following_trajectory = true;
+}
+
+
--- Robot.h
+++ Robot.h
@@ -3,30 +3,54 @@
 #include <Windows.h>
 #include <gdiplus.h>
 #include <cmath>
+#include <Eigen/dense>
+#include <vector>
 
 #include "Triangle.h"
 
 
 using namespace Gdiplus;
+using namespace Eigen;
 
 inline REAL abs(const PointF& v)
 {
 	return std::sqrt(v.X * v.X + v.Y * v.Y);
 }
 
+enum RobotCommand
+{
+	rc_stop, rc_release, rc_catch, rc_do_nothing
+};
+
+struct RobotPosition
+{
+	Point pos;
+	RobotCommand robotCommand;
+};
+
 class Robot
 {
-private:
+public:
 	REAL arm_width = 5;
 	Pen pen1, pen2;
 	PointF base_pos, arm1, arm2;
-	
+	float angle1, angle2, tAngle1, tAngle2;
+	float ang_vel1 = 0, ang_vel2 = 0;
+	float omega1 = 0.005, omega2 = 0.002;
+	float dzeta1 = 0.6, dzeta2 = 0.6;
+	bool stopped, tpos_reached;
+	bool following_trajectory;
+	std::vector<RobotPosition> trajectory;
+	std::vector<RobotPosition>::const_iterator curr_tpos;
+
+	void follow_trajectory();
+
 public:
 	Triangle* catched_triangle = nullptr;
 	PointF catched_triangle_r;
-	REAL arm1_length, arm2_length, angle1_deg, angle2_deg;
+	REAL arm1_length, arm2_length;
 
-	Robot(REAL arm1_length_ = 300, REAL arm2_length_ = 300, REAL angle1_deg_ = -45, REAL angle2_deg_ = 45);
+	Robot(REAL arm1_length_ = 300, REAL arm2_length_ = 300, REAL angle1_deg_ = 90, REAL angle2_deg_ = 0);
 
 	void draw(Graphics* graphics);
 
@@ -36,9 +60,23 @@ public:
 
 	void set_arms(REAL angle1, REAL angle2);
 
+	void set_tAngles(float a1, float a2);
+
+	void set_tAngle1(float a) { tAngle1 = a; }
+
+	void set_tAngle2(float a) { tAngle2 = a; }
+
 	void set_postion(PointF p);
+
+	void set_tPosition(PointF);
+
+	Vector2f calculateAngles(PointF);
+
+	void enter_trajectory(const std::vector<RobotPosition>&);
 
 	PointF get_positon();
 };
+
+
 
 #endif // !ROBOT_H
